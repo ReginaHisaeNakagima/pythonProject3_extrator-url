773e8c065086fc4dc44ca456d3fed1349eaf6462
HisaeNakagima <1823289@aluno.univesp.br>
2023-08-30 12:08:17 -0300

    Utilizando expressões regulares

--- extrator_url.py
+++ extrator_url.py
@@ -33,8 +33,7 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
-
-
 extrator_url = ExtratorURL(None)
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
+
